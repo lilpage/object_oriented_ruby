6fad76660b0af82624150510c0d9d36db41880f8
Lil Page <lillehpage@gmail.com>
2021-02-17 12:29:09 -0600

    Fixes symbol grammar

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,4 @@
-inventory1 = {element : "neon", number : 10, price : 25.50}
-inventory2 = {element : "barium", number : 56, price : 60}
-inventory3 = {element : "magnesium", number : 12, price : 14.50}
+inventory1 = {element: "neon", number: 10, price: 25.50}
+inventory2 = {element: "barium", number: 56, price: 60}
+inventory3 = {element: "magnesium", number: 12, price: 14.50}
+p inventory1[:element]
