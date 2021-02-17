4ad8d4062a8460965f796d929310f60984fc9f2f
Lil Page <lillehpage@gmail.com>
2021-02-17 12:27:11 -0600

    Creates inventory

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+inventory1 = {element : "neon", number : 10, price : 25.50}
+inventory2 = {element : "barium", number : 56, price : 60}
+inventory3 = {element : "magnesium", number : 12, price : 14.50}
