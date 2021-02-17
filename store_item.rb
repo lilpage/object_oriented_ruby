a2351bcecea7cc49f55d2771c32da1a4c68aa8e6
Lil Page <lillehpage@gmail.com>
2021-02-17 13:34:50 -0600

    Adds writer method

--- store_item.rb
+++ store_item.rb
@@ -1,8 +1,8 @@
 # as a hash
-inventory1 = {element: "neon", number: 10, price: 25.50}
-inventory2 = {element: "barium", number: 56, price: 60}
-inventory3 = {element: "magnesium", number: 12, price: 14.50}
-p inventory1[:element]
+# inventory1 = {element: "neon", number: 10, price: 25.50}
+# inventory2 = {element: "barium", number: 56, price: 60}
+# inventory3 = {element: "magnesium", number: 12, price: 14.50}
+# p inventory1[:element]
 
 # as a class
 class Inventory 
@@ -25,4 +25,11 @@ class Inventory
     @price
   end
 
+  def price=(num) #writer
+    @price = num
+  end
+
 end
+
+inventory1 = Inventory.new("carbon", 6, 2.25)
+puts inventory1
